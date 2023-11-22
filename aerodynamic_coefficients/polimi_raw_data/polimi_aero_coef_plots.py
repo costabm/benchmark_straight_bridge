f6ab94d55576acbf2359a327ef715075c3c8c524
costabm <bernamdc@gmail.com>
2023-11-22 19:33:23 +0100

    Ben commit

--- aerodynamic_coefficients/polimi_raw_data/polimi_aero_coef_plots.py
+++ aerodynamic_coefficients/polimi_raw_data/polimi_aero_coef_plots.py
@@ -3,52 +3,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-
-which_tests = 'K12_G_L_T1'  # K12_G_L_no_traffic_sign ; K12_G_L_with_traffic_sign ; K12_G_L_T1
+folder_name = os.path.join(os.getcwd(), r'aerodynamic_coefficients\polimi_raw_data')
+polimi_file_name = r'ResultsCoefficients-Rev1.xlsx'
+polimi_sheet_name = r'K12-G-L'
+phase7_file_name = 'aero_coefs_in_Ls_from_SOH_CFD_scaled_to_Julsund.xlsx'  # Beginning of Phase 7, prelim. coefficients
 
 # Load results
-path_polimi_data = os.path.join(os.getcwd(), r'aerodynamic_coefficients\polimi_raw_data/coefficients.xlsx')
-df_polimi = pd.read_excel(io=path_polimi_data, sheet_name='coeff ' + which_tests).dropna().sort_values(['yaw', 'theta'])
+path_polimi_data = os.path.join(folder_name, polimi_file_name)
+df_polimi = pd.read_excel(io=path_polimi_data, sheet_name=polimi_sheet_name).dropna().sort_values(['Yaw', 'Theta'])
 
 # Renaming coefficients
-df_polimi.rename(columns={'CMxL':'CrxL', 'CMxi':'Crxi',
-                          'CMyL':'CryL', 'CMyi':'Cryi',
-                          'CMzL':'CrzL', 'CMzi':'Crzi'}, inplace=True)
+df_polimi.rename(columns={'Code': 'code', 'qRef': 'q_ref', 'Yaw': 'yaw', 'Theta': 'theta',
+                          'CMxL': 'CrxL', 'CMxi': 'Crxi',
+                          'CMyL': 'CryL', 'CMyi': 'Cryi',
+                          'CMzL': 'CrzL', 'CMzi': 'Crzi',
+                          'CxTot':   'Cx', 'CyTot':   'Cy', 'CzTot':   'Cz',
+                          'CMxTot': 'Crx', 'CMyTot': 'Cry', 'CMzTot': 'Crz'}, inplace=True)
 
-# Multiplying Cx by (-1)
-df_polimi['CxL'] = df_polimi['CxL'] * -1
-df_polimi['Cxi'] = df_polimi['Cxi'] * -1
+# Collecting the coefficients used in the start of Phase 7
+phase7_file_path = os.path.join(folder_name, phase7_file_name)
+C_phase7_table = {dof: pd.read_excel(phase7_file_path, header=None, sheet_name=dof).to_numpy()
+                  for dof in ['Cx', 'Cy', 'Cz', 'Crx', 'Cry', 'Crz']}
 
-
-path_phase7_data = os.path.join(os.getcwd(), r'aerodynamic_coefficients\polimi_raw_data\aero_coefs_in_Ls_from_SOH_CFD_scaled_to_Julsund.xlsx')  # First part of the Phase 7, with the preliminary coefficients
-C_phase7_table = {
-    'Cx' : pd.read_excel(path_phase7_data, header=None, sheet_name='Cx').to_numpy(),
-    'Cy' : pd.read_excel(path_phase7_data, header=None, sheet_name='Cy').to_numpy(),
-    'Cz' : pd.read_excel(path_phase7_data, header=None, sheet_name='Cz').to_numpy(),
-    'Crx': pd.read_excel(path_phase7_data, header=None, sheet_name='Crx').to_numpy(),
-    'Cry': pd.read_excel(path_phase7_data, header=None, sheet_name='Cry').to_numpy(),
-    'Crz': pd.read_excel(path_phase7_data, header=None, sheet_name='Crz').to_numpy()}
-
-# Create new results from the average of both sensors
-df_polimi['Cx'] = (df_polimi['CxL'] + df_polimi['Cxi']) / 2
-df_polimi['Cy'] = (df_polimi['CyL'] + df_polimi['Cyi']) / 2
-df_polimi['Cz'] = (df_polimi['CzL'] + df_polimi['Czi']) / 2
-df_polimi['Crx'] = (df_polimi['CrxL'] + df_polimi['Crxi']) / 2
-df_polimi['Cry'] = (df_polimi['CryL'] + df_polimi['Cryi']) / 2
-df_polimi['Crz'] = (df_polimi['CrzL'] + df_polimi['Crzi']) / 2
-
-lst_betas = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+lst_betas = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]  # list of betas to plot
 
 for str_coef in ['Cx', 'Cy', 'Cz', 'Crx', 'Cry', 'Crz']:
     plt.figure(dpi=400)
     for i, beta in enumerate(lst_betas):
-        color_list = plt.cm.plasma(np.linspace(0, 0.95, len(lst_betas)-1)).tolist()
+        color_list = plt.cm.plasma(np.linspace(0, 0.95, len(lst_betas) - 1)).tolist()
         color_list.append([0.5, 0.5, 0.5, 1])  # add a black color for the markers at beta 90 deg
 
         # Polimi
-        thetas_polimi = df_polimi[df_polimi['yaw']==beta]['theta']
-        C_polimi = df_polimi[df_polimi['yaw']==beta][str_coef]
-        C_polimi_L = df_polimi[df_polimi["yaw"] == beta][str_coef+'L']  # left sensor "L"
+        thetas_polimi = df_polimi[df_polimi['yaw'] == beta]['theta']
+        C_polimi = df_polimi[df_polimi['yaw'] == beta][str_coef]
+        C_polimi_L = df_polimi[df_polimi["yaw"] == beta][str_coef + 'L']  # left sensor "L"
         C_polimi_i = df_polimi[df_polimi["yaw"] == beta][str_coef + 'i']  # right sensor "i"
 
         # Phase 7A coefficients
@@ -71,9 +59,5 @@ for str_coef in ['Cx', 'Cy', 'Cz', 'Crx', 'Cry', 'Crz']:
     plt.ylabel(str_coef)
     plt.xlabel(r'Theta [$\theta$]')
     plt.grid()
-    plt.savefig(r"aerodynamic_coefficients\polimi_raw_data\polimi_" + which_tests + '_' + str_coef + ".jpg")
+    plt.savefig(folder_name + r"\plots\polimi_" + polimi_sheet_name + '_' + str_coef + ".jpg")
     plt.close()
-
-
-
-
--- aerodynamic_coefficients/polimi_raw_data/polimi_raw_data.py
+++ aerodynamic_coefficients/polimi_raw_data/polimi_raw_data.py
@@ -1,4 +1,11 @@
-import copy
+"""
+In late 2023, Politecnico di Milano (Polimi) delivered us new wind tunnel test results in skew winds.
+This script allows pre- and post-processing the raw data files provided.
+
+bercos@vegvesen.no
+November 2023
+"""
+
 import os
 import numpy as np
 import matplotlib
@@ -7,9 +14,17 @@ from mat4py import loadmat
 from scipy.optimize import curve_fit
 from my_utils import root_dir, get_list_of_colors_matching_list_of_objects, fill_with_None_where_repeated
 import pandas as pd
-matplotlib.use('Qt5Agg')
+matplotlib.use('Qt5Agg')  # to prevent bug in PyCharm
 
 folder_path_to_raw_data = os.path.join(root_dir, r"aerodynamic_coefficients\polimi_raw_data\preliminary")
+
+# Common variables
+scale = 1/35  # model scale
+
+# Pontoon dimensions (for normalization)
+H_pont = np.round(3.5 * scale, 10)
+B_pont = 14.875 * scale
+L_pont = 53 * scale
 
 
 def get_raw_data_dict(folder_path):
@@ -37,9 +52,10 @@ def get_raw_data_dict(folder_path):
             data[k1] = {}  # prepare for a nested dict
             for k2 in raw_file:  # collecting for all keys in the raw file
                 data[k1][k2] = raw_file[k2]
-            u_ceiling = np.sqrt(data[k1]['qCeiling'] / (1/2*data[k1]['rho']))
+            u_ceil = np.sqrt(data[k1]['qCeiling'] / (1/2*data[k1]['rho']))
             polimi_yaw = float(file_rel_path.split('_Ang')[1].split('.mat')[0])  # find string between substrings
-            data[k1].update({'u_ceiling': u_ceiling, 'U_ceiling': np.mean(u_ceiling), 'polimi_yaw': polimi_yaw})
+            data[k1].update({'u_ceil': u_ceil, 'U_ceil': np.mean(u_ceil), 'polimi_yaw': polimi_yaw})
+            data[k1]['q_ceil'] = data[k1].pop('qCeiling')  # rename key, ditching old one
 
             # Collect data unique to each raw_data_type
             if raw_data_type == 'coh':
@@ -49,11 +65,17 @@ def get_raw_data_dict(folder_path):
                 U = np.mean(u, axis=1)
                 data[k1].update({'U': U, 'u': u, 'v': v, 'w': w})  # updating dict
             if raw_data_type in ['pont', 'deck']:
-                data[k1]['THForces'] = np.array(data[k1]['THForces']).T  # new shape: (dof, n_samples)
-                data[k1]['THForces_mean'] = np.mean(data[k1]['THForces'], axis=1)  # new shape: (dof, n_samples)
+                data[k1]['F'] = np.array(data[k1]['THForces']).T  # new shape: (dof, n_samples)
+                del data[k1]['THForces']  # ditching old key
+                data[k1]['units'] = data[k1].pop('EU')  # rename key, ditching old one
+                data[k1]['dof_tag'] = data[k1].pop('names')  # rename key, ditching old one
+                data[k1]['F_mean'] = np.mean(data[k1]['F'], axis=1)  # new shape: (dof, n_samples)
             if raw_data_type == 'deck':
                 data[k1]['upwind_uvw'] = np.array(data[k1]['upwind_uvw']).T  # new shape: (3, n_samples)
                 data[k1]['upwind_U'] = np.mean(data[k1]['upwind_uvw'][0])
+                data[k1]['q_ref'] = data[k1].pop('qRef')  # rename key, ditching old one
+            if raw_data_type == 'pont':
+                data[k1]['q_tilde_ref'] = data[k1].pop('qTildeRef')  # rename key, ditching old one
         return data
 
     coh_data = get_raw_data_dict_from_file_paths(coh_file_paths, raw_data_type='coh')
@@ -62,25 +84,25 @@ def get_raw_data_dict(folder_path):
 
     return {'coh_data': coh_data,
             'pont_data': pont_data,
-            'deck_data': deck_data}  # todo: complete the remaining data
+            'deck_data': deck_data}
 
 
 def get_dfs_from_raw_data(raw_data_dict, drop_time_series=True):
     """
-    Takes as input a triply nested dictionary, with the outer keys given in the first assertion below
+    Takes as input a triply nested dictionary, with the outer keys as 'coh_data', 'pont_data' and 'deck_data'.
     drop_time_series: drops the large time series, for efficiency and to get a dataframe with non-iterable cells
     """
     assert list(raw_data_dict.keys()) == ['coh_data', 'pont_data', 'deck_data'], 'new raw_data_type is not implemented!'
 
     def get_df_from_raw_data(raw_data):
         """
-        The dataframe processing that is common to all raw_data_type is performed in this function
+        Dataframe processing that is common to all raw_data_type
         raw_data: doubly nested dictionary
         """
         df = pd.DataFrame.from_dict(raw_data).transpose()
         if drop_time_series:
-            df = df.drop(['qCeiling', 'u', 'v', 'w', 't', 'u_ceiling',
-                            'THForces', 'upwind_uvw'], axis=1, errors='ignore')  # ignore already non-existing keys
+            df = df.drop(['q_ceil', 'u', 'v', 'w', 't', 'u_ceil',
+                            'F', 'upwind_uvw'], axis=1, errors='ignore')  # ignore already non-existing keys
         return df.reset_index(names='case_tag')
 
     coh_df = get_df_from_raw_data(raw_data_dict['coh_data'])
@@ -89,8 +111,8 @@ def get_dfs_from_raw_data(raw_data_dict, drop_time_series=True):
 
     # These cols have a list on each cell. Exploding lists to multiple rows
     coh_df = coh_df.explode(['cobras', 'U'])
-    pont_df = pont_df.explode(['EU', 'names', 'THForces_mean'])
-    deck_df = deck_df.explode(['EU', 'names', 'THForces_mean'])
+    pont_df = pont_df.explode(['units', 'dof_tag', 'F_mean'])
+    deck_df = deck_df.explode(['units', 'dof_tag', 'F_mean'])
 
     return coh_df, pont_df, deck_df
 
@@ -100,18 +122,21 @@ raw_data_dict = get_raw_data_dict(folder_path_to_raw_data)
 coh_df, pont_df, deck_df = get_dfs_from_raw_data(raw_data_dict)
 
 # Further post-processing
-coh_df['U/U_ceiling'] = coh_df['U'] /coh_df['U_ceiling']
+coh_df['U/U_ceil'] = coh_df['U'] / coh_df['U_ceil']
 
 
-# Plotting
+# The following plots assess if we should have yaw-dependent coefficients or not. Conclusion: not
 def plot_for_cobras():
+    """
+    Plot normalized U with the cobra ID in the x_axis
+    """
     x_axis = 'cobras'
     label_axis = 'yaw'
     coh_df['colors_to_plot'] = get_list_of_colors_matching_list_of_objects(coh_df[label_axis])
     plt.figure(dpi=400)
     for label, color in dict(zip(coh_df[label_axis], coh_df['colors_to_plot'])).items():
         sub_df = coh_df[coh_df[label_axis] == label]  # subset dataframe
-        plt.scatter(sub_df[x_axis], sub_df['U/U_ceiling'], c=sub_df['colors_to_plot'], label=label, alpha=0.8)
+        plt.scatter(sub_df[x_axis], sub_df['U/U_ceil'], c=sub_df['colors_to_plot'], label=label, alpha=0.8)
     plt.legend(title='yaw [deg]', bbox_to_anchor=(1.04, 0.5), loc="lower left")
     plt.ylabel('$U_{centre}\//\/U_{ceiling}$')
     plt.grid()
@@ -122,6 +147,9 @@ def plot_for_cobras():
 
 
 def plot_for_yaw():
+    """
+    Plot normalized U with the yaw angle in the x_axis
+    """
     x_axis = 'yaw'
     label_axis = 'cobras'
     coh_df['colors_to_plot'] = get_list_of_colors_matching_list_of_objects(coh_df[label_axis])
@@ -129,7 +157,7 @@ def plot_for_yaw():
     plt.title('(obtained from the coherence measurement tests)')
     for label, color in dict(zip(coh_df[label_axis], coh_df['colors_to_plot'])).items():
         sub_df = coh_df[coh_df[label_axis] == label]  # subset dataframe
-        plt.scatter(sub_df[x_axis], sub_df['U/U_ceiling'], c=sub_df['colors_to_plot'], label=label, alpha=0.8)
+        plt.scatter(sub_df[x_axis], sub_df['U/U_ceil'], c=sub_df['colors_to_plot'], label=label, alpha=0.8)
     plt.scatter([0,30,60,90],
                 [0.886, 0.839, 0.833, 0.851],
                 marker='x', color='black', label='(from wind profile tests)')
@@ -147,77 +175,105 @@ def plot_for_yaw():
 # plot_for_yaw()
 
 
-def func_to_fit(z, a, b):
-    return a * np.log(z / b)
-
-
-# Calculating the coefficients from force measurements
-scale = 1/35
-dof = 'Fx'
+# Calculating the coefficients from force measurements. For this, the wind profile needs to be established, because
+# the wind forces are to be normalized by the integrated / averaged wind speed along the pontoon / column height
+scale = 1/35  # wind-tunnel model scale
+dof = 'Fy'  #
 yaw = 180
-H = np.round(3.5 * scale, 10)
-B = 14.875 * scale
-L = 53 * scale
-U_profile_raw = np.array([[1E-5, 1E-5],
-                          [0.03, 8.3398],
-                          [0.05, 8.4902],
-                          [0.1, 8.6605],
-                          [0.15, 8.9762],
-                          [0.2, 9.2538],
-                          [0.25, 9.4597],
-                          [0.3, 9.854],
-                          [0.35, 9.918],
-                          [0.4, 10.0355],
-                          [0.46, 10.2358],
-                          [0.5, 10.4923],
-                          [0.6, 10.5613],
-                          [0.7, 10.7889]])
-x_raw = U_profile_raw[:, 0]
-y_raw = U_profile_raw[:, 1]
-x_fit = np.linspace(x_raw.min(), x_raw.max(), num=1E6)
-y_interp = np.interp(x=x_fit, xp=x_raw, fp=y_raw)
-
-popt, pcov, *_ = curve_fit(f=func_to_fit, xdata=x_raw, ydata=y_raw, bounds=np.array([[0, 0], [np.inf, np.inf]]))
-y_fit = func_to_fit(x_fit, *popt)
-
-plt.scatter(x_raw, y_raw, label='raw data')
-# plt.plot(x_interp, y_interp, label='interpolation')
-plt.plot(x_fit, y_fit, label='curve fit')
-plt.legend()
-plt.show()
-
-# Integrating raw (coarser) data
-idx = np.where(x_raw <= H)[0][-1]
-y_raw_ref = np.trapz(y=y_raw[:idx+1], x=x_raw[:idx+1]) / H
-
-# Integrating interpolated (finer) data
-idx = np.where(x_fit <= H)[0][-1]
-y_interp_ref = np.trapz(y=y_interp[:idx+1], x=x_fit[:idx+1]) / H
-
-# Integrating fitted (finer) data
-idx = np.where(x_fit <= H)[0][-1]
-y_fit_ref = np.trapz(y=y_fit[:idx+1], x=x_fit[:idx+1]) / H
-
-U_ref = copy.deepcopy(y_fit_ref)
-
-sub_df = pont_df[(pont_df['names']==dof+'-PTot') & (pont_df['yaw']==yaw)]  # subset of df
-
-rho = sub_df['rho']
-qref_tilde = 1/2 * rho * 7.596**2
-
-Fx = sub_df['THForces_mean']
 
 
-Cx = Fx / (qref_tilde * L * H)
+# This function is to be re-done when we get the final raw data files
+def get_raw_aero_coef(dof, yaw, where='pont', back_engineered_U=False):
+    """
+    Describe here....
+    at: 'pont', 'deck', ...
+    """
+
+    #   The wind profile values were taken from a draft raw data Excel file provided below:
+    #   https://vegvesen.sharepoint.com/:x:/s/arb-bjffeedwindtunneltests/EVehciF9YZJEoTX42t6B2qkBSay4agGUy_8MMMQHn9lntA?email=bernardo.morais.da.costa%40vegvesen.no&e=BwZEry
+    # Wind profile, required for force normalization
+    U_profile_raw = np.array([[1E-5, 1E-5],
+                              [0.03, 8.3398],
+                              [0.05, 8.4902],
+                              [0.10, 8.6605],
+                              [0.15, 8.9762],
+                              [0.20, 9.2538],
+                              [0.25, 9.4597],
+                              [0.30, 9.854],
+                              [0.35, 9.918],
+                              [0.40, 10.0355],
+                              [0.46, 10.2358],
+                              [0.50, 10.4923],
+                              [0.60, 10.5613],
+                              [0.70, 10.7889]])
+    x_raw = U_profile_raw[:, 0]
+    y_raw = U_profile_raw[:, 1]
+
+    def func_to_fit(z, a, b):
+        """
+        this logarithmic function is used to fit the measurements of the wind profile (with parameters 'a' and 'b')
+        """
+        return a * np.log(z / b)
+
+    def get_fitted_wind_profile(plot=False):
+        """
+        x_fit: Used for the height above ground (z) [m]
+        return: the fitted wind speed [m/s]
+        """
+        x_fit = np.linspace(x_raw.min(), x_raw.max(), num=1000000)
+        y_interp = np.interp(x=x_fit, xp=x_raw, fp=y_raw)
+        popt, pcov, *_ = curve_fit(f=func_to_fit, xdata=x_raw, ydata=y_raw, bounds=np.array([[0, 0], [np.inf, np.inf]]))
+        y_fit = func_to_fit(x_fit, *popt)
+        if plot:
+            # Plotting the fitted logarithm
+            plt.scatter(x_raw, y_raw, label='raw data')
+            plt.plot(x_fit, y_interp, label='interpolation')
+            plt.plot(x_fit, y_fit, label='curve fit')
+            plt.legend()
+            plt.show()
+        return x_fit, y_fit
+
+    x_fit, y_fit = get_fitted_wind_profile()
+
+    # # Integrating raw (coarser) data
+    # idx = np.where(x_raw <= H)[0][-1]
+    # y_raw_ref = np.trapz(y=y_raw[:idx+1], x=x_raw[:idx+1]) / H
+
+    # # Integrating interpolated (finer) data
+    # idx = np.where(x_fit <= H)[0][-1]
+    # y_interp_ref = np.trapz(y=y_interp[:idx+1], x=x_fit[:idx+1]) / H
+
+    if where == 'pont':
+        H = H_pont
+        B = B_pont
+        L = L_pont
+        sub_df = pont_df[(pont_df['dof_tag'] == dof + '-PTot') & (pont_df['yaw'] == yaw)]  # subset of df
+
+    else:
+        assert where == 'deck', "Error: Haven't implemented coefficients of other elements"
+        H = np.round(3.5 * scale, 10)
+        sub_df = deck_df[(deck_df['dof_tag'] == dof + '-Tot') & (deck_df['yaw'] == yaw)]  # subset of df
+
+
+    if not back_engineered_U:
+        # Integrating fitted (finer) data
+        idx = np.where(x_fit <= H)[0][-1]
+        U_ref = np.trapz(y=y_fit[:idx+1], x=x_fit[:idx+1]) / H
+    else:  # todo: to be deleted, used just for testing
+        U_ref = 7.596  # the speed that gives the q-tilde-ref in the Excel file of Polimi
+    qref_tilde = 1 / 2 * rho * U_ref ** 2
+    F = sub_df['F_mean']  # force or moment
+
+    rho = sub_df['rho']
 
 
 
+    # BERNARDO RE-CODE ALL THIS. SEE ANNEX-DRAFT.PDF AND THE FORMULAS OF EACH COEFFICIENT FOR THE PONTOON AND DECK
+    # if dof in ['Fx', 'Fy', 'Fz']:
+    #
+    #     C = F / (qref_tilde * L * H)
+    # else:
+    #     assert dof in ['Mx', 'My', 'Mz']
 
+    return NotImplementedError
 
-# # todo: TELL POLIMI ABOUT:
-print(" len(t) is 120000, but len(u)=len(v)=len(w) is 119808. File example: J-1658_SIW_FLOW-MD_P7700_Ang-180.mat  ")
-print("Polimi hasn't delivered the wind profile at column and pontoon locations yet?")
-print("No upwind_uvw information in the pontoon data? (Understandable that it is not in the coherence data)")
-print("It would be very useful to have the data from the upwind pitot tube for all measurements")
-print("If the Polimi yaw_angle is kept in the file names, then we must know if the data provided is already"
-      "transformed to our yaw definition (by multiplying by -1 where needed) or not")
